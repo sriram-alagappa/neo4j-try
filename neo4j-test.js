d73b6ae9ed914962d94fcc23f233821e7e335990
sriram <salagappa@provocraft.com>
2016-02-07 11:23:30 -0700

    .

--- neo4j-test.js
+++ neo4j-test.js
@@ -1,5 +1,31 @@
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('*');
+
+var neo4j = require('neo4j');//https://github.com/thingdom/
+var db = new neo4j.GraphDatabase('');
+
+// CREATE (n {name:"World"}) 
+// CREATE (n:Person { name : 'Chris', job_title : 'Developer'})
+
+// create (n:Person{name:"sriram"})
+// create (n:Project{name:"make it now",time:"30 min"})
+
+/*
+
+Match(p:Person {name:"sriram"}),(pr:Project{name:"make it now"})
+create (p) - [:LIKES] ->(pr)
+return p,pr
+
+
+
+Match (n)
+optional match (n) - [r]-()
+return n,r
+
+
+*/
+
+
+
+
 
 db.cypher({
     query: 'MATCH (n) RETURN n LIMIT 100'
